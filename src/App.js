c0bc98486297cd50c36cb5f623bb2559a17c7ef5
Sunny Matharu <sunnymatharuuk@gmail.com>
2020-06-23 15:36:01 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import React from 'react';
 import { Client } from 'boardgame.io/react';
+import { Local } from 'boardgame.io/multiplayer';
+import { SocketIO } from 'boardgame.io/multiplayer'
 import TicTacToe from './Game';
-import Board from "./Board";
+import TicTacToeBoard from './Board';
 
-const App = Client({ 
+const TicTacToeClient = Client({
   game: TicTacToe,
-  board: Board
+  board: TicTacToeBoard,
+  //multiplayer: Local(),
+  multiplayer: SocketIO({ server: 'localhost:8000' }),
 });
+
+// const App = () => (
+//   <div>
+//     <TicTacToeClient playerID="0" />
+//     <TicTacToeClient playerID="1" />
+//   </div>
+// );
+
+
+
+class App extends React.Component {
+  state = { playerID: null };
+
+  render() {
+    if (this.state.playerID === null) {
+      return (
+        <div>
+          <p>Play as</p>
+          <button onClick={() => this.setState({ playerID: "0" })}>
+            Player 0
+          </button>
+          <button onClick={() => this.setState({ playerID: "1" })}>
+            Player 1
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <TicTacToeClient playerID={this.state.playerID} />
+      </div>
+    );
+  }
+}
 
 export default App;
